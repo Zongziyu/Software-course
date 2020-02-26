e25dbbc3fd5004cc50dc85928669c7dd753bf5ae
JuePet <60561310+JuePet@users.noreply.github.com>
2020-02-26 16:23:43 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,7 +3,9 @@ package FightAgainstLandlords;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class Player {
 	public String role;
@@ -66,9 +68,190 @@ public class Player {
 	}
 	
 	//出牌提示
-	public List<Card> HowToPlay(List<Card> last_cards){
-		List<Card> PlayCard =  new ArrayList<Card>();
-		return PlayCard;
+	public List<Card> HowToPlay(List<Card> last_cards,List<Card> my_cards){
+		List<Card> PlayCards =  new ArrayList<Card>();
+		//整理重复的牌
+		HashMap<Integer,Integer> last_hash = new HashMap<Integer,Integer>();
+		HashMap<Integer,Integer> my_hash = new HashMap<Integer,Integer>();
+		for(Card card:last_cards) {
+			if(last_hash.get(card.GetNum()) != null) {
+				Integer integer = last_hash.get(card.GetNum());
+				last_hash.put(card.GetNum(),integer + 1);
+			}
+			else {
+				last_hash.put(card.GetNum(), 1);
+			}
+		}
+		for(Card card:my_cards) {
+			if(my_hash.get(card.GetNum()) != null) {
+				Integer integer = my_hash.get(card.GetNum());
+				my_hash.put(card.GetNum(),integer + 1);
+			}
+			else {
+				my_hash.put(card.GetNum(), 1);
+			}
+		}
+		
+		int last_number = last_cards.size();
+		boolean can_play = false;//表示是否有牌可出
+		if(last_number == 1) {
+			for(Card card:my_cards) {
+				if(card.GetNum() > last_cards.get(0).GetNum()) {
+					PlayCards.add(card);
+					can_play = true;
+					break;
+				}
+			}
+		}
+		//对
+		else if(last_number == 2 && last_hash.size() == 1) {
+			for(int i = 0;i < my_cards.size() - 1;i++) {
+				if(my_cards.get(i).GetNum() > last_cards.get(0).GetNum()
+						&& my_cards.get(i + 1).GetNum() == my_cards.get(i).GetNum()) {
+					PlayCards.add(my_cards.get(i));
+					PlayCards.add(my_cards.get(i+1));
+					can_play = true;
+					break;
+				}
+			}
+		}
+		//3带
+		else if((last_number == 4 && last_hash.size() == 2) || last_number == 5 && last_hash.size() == 2) {
+			int usecard = 0;//3带中的带
+			for(Entry<Integer,Integer> entry : last_hash.entrySet()) {
+				if(entry.getValue() == 3) {
+					usecard = entry.getKey();
+				}
+			}
+			for(int i = 0;i < my_cards.size() - 2;i++) {
+				if(my_cards.get(i).GetNum() > usecard && my_cards.get(i).GetNum() == my_cards.get(i+1).GetNum()
+						 && my_cards.get(i+1).GetNum() == my_cards.get(i+2).GetNum()) {
+					PlayCards.add(my_cards.get(i));
+					PlayCards.add(my_cards.get(i+1));
+					PlayCards.add(my_cards.get(i+2));
+					if(last_number == 4) {
+						if(i != 0) {
+							PlayCards.add(my_cards.get(0));
+						}
+						else {
+							PlayCards.add(my_cards.get(3));
+						}
+					}
+					else {
+						for(int j = 0;j < my_cards.size() - 1;j++)
+						{
+							if(j < i - 1 || j > i + 2) {
+								if(my_cards.get(j).GetNum() == my_cards.get(j+1).GetNum()) {
+									PlayCards.add(my_cards.get(j));
+									PlayCards.add(my_cards.get(j + 1));
+								}
+							}
+						}
+					}
+					break;
+				}
+			}
+			can_play = true;
+		}
+		//顺子
+		else if(last_number >= 6) {
+			if(last_number / last_hash.size() == 2) {
+				int now_number = 0;
+				int i = 0;
+				while(i < my_cards.size() - 2) {
+					if(my_cards.get(i).GetNum() == my_cards.get(i + 1).GetNum()
+							&& my_cards.get(i + 2).GetNum() == my_cards.get(i+1).GetNum() + 1 &&
+							my_cards.get(i).GetNum() > last_cards.get(0).GetNum()) {
+						PlayCards.add(my_cards.get(i));
+						PlayCards.add(my_cards.get(i+1));
+						PlayCards.add(my_cards.get(i+2));
+						now_number+=3;
+					}
+					else {
+						PlayCards.clear();
+						now_number = 0;
+						i -= 1;
+					}
+					i+=2;
+					if(now_number == last_number) {
+						can_play = true;
+						break;
+					}
+				}
+			}
+			else {
+				int i = 0;
+				int now_number = 0;
+				while(i < last_number - 1) {
+					if(my_cards.get(i).GetNum() + 1 == my_cards.get(i + 1).GetNum() &&
+							my_cards.get(i).GetNum() > last_cards.get(0).GetNum()) {
+						now_number += 1;
+						PlayCards.add(my_cards.get(i));
+					}
+					else {
+						now_number = 0;
+						PlayCards.clear();
+					}
+					if(now_number == last_number) {
+						can_play = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		//出炸弹
+		if(can_play == false) {
+			//普通炸弹
+			for(int i = 0;i < my_cards.size() - 3;i++) {
+				if(my_cards.get(i).GetNum() == my_cards.get(i + 1).GetNum() &&
+						my_cards.get(i + 1).GetNum() == my_cards.get(i + 2).GetNum() &&
+						my_cards.get(i + 2).GetNum() == my_cards.get(i + 3).GetNum()) {
+					for(int j = i;j < i + 4;j++) {
+						PlayCards.add(my_cards.get(j));
+						can_play = true;
+					}
+					break;
+				}
+			}
+			
+			//王炸
+			if(can_play == false) {
+				if(my_cards.get(my_cards.size() - 1).GetCol() == 4 && 
+						my_cards.get(my_cards.size() - 2).GetCol() == 4) {
+					PlayCards.add(my_cards.get(my_cards.size() - 1));
+					PlayCards.add(my_cards.get(my_cards.size() - 2));
+				}
+			}
+		}
+		
+		//炸弹对炸弹
+		if(last_number == 4 && last_hash.size() == 1) {
+			int lcd = last_cards.get(0).GetNum();
+			boolean ifbig = false;
+			for(int i = 0;i < my_cards.size() - 3;i++) {
+				if(my_cards.get(i).GetNum() == my_cards.get(i + 1).GetNum() &&
+						my_cards.get(i + 1).GetNum() == my_cards.get(i + 2).GetNum() &&
+						my_cards.get(i + 2).GetNum() == my_cards.get(i + 3).GetNum() &&
+						my_cards.get(i).GetNum() > lcd) {
+					for(int j = i;j < i + 4;j++) {
+						PlayCards.add(my_cards.get(j));
+						ifbig = true;
+					}
+					break;
+				}
+			}
+			
+			//王炸
+			if(ifbig == false) {
+				if(my_cards.get(my_cards.size() - 1).GetCol() == 4 && 
+						my_cards.get(my_cards.size() - 2).GetCol() == 4) {
+					PlayCards.add(my_cards.get(my_cards.size() - 1));
+					PlayCards.add(my_cards.get(my_cards.size() - 2));
+				}
+			}
+		}
+		return PlayCards;
 	}
 }
 
